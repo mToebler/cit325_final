ca247929acd33093b0ce09449283f19ddc0f7f88
Mark Tobler <marktobler@gmail.com>
2020-04-04 03:31:10 -0700

    working 1.312

--- elf_t.sql
+++ elf_t.sql
@@ -10,7 +10,7 @@ BEGIN
    FOR i IN (SELECT   object_name
             ,        object_type
             FROM     user_objects
-            WHERE    REGEXP_LIKE(object_name,'^elf_t.*$','i')
+            WHERE    object_name IN ('ELF_T', 'SILVAN_T', 'SINDAR_T', 'NOLDOR_T', 'TELERI_T'))
             ORDER BY 2 DESC) LOOP
       IF i.object_type = 'TYPE' THEN
          EXECUTE IMMEDIATE 'DROP TYPE '||i.object_name||' FORCE';
